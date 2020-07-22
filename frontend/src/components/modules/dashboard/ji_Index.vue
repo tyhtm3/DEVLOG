90ffd1676ecedae10dfc05b310137de6df336821
myeongji <ji_exitosa@naver.com>
2020-07-22 22:03:29 +0900

    S03P12A402-33 Add tab menu to blog-main

--- frontend/src/components/modules/dashboard/ji_Index.vue
+++ frontend/src/components/modules/dashboard/ji_Index.vue
@@ -1,59 +1,30 @@
 <template>
     <transition name="el-zoom-in-top">
-        <!-- <div class="content-wrapper dashboard-wrapper">
-            <div class="col-sm-12">
-                <br><br><br>
-                <div class="row">
-                  <div class="col-xs-12 col-sm-4 emphasis_custom">
-                    <a href="#"><h2><strong>Project</strong></h2></a>
-                  </div>
-                  <div class="col-xs-12 col-sm-4 emphasis_custom">
-                    <a href="#"><h2><strong>Post</strong></h2></a>
-                  </div>
-                  <div class="col-sm-4 emphasis_custom">
-                    <a href="#"><h2><strong>Portfolio</strong></h2></a>
-                  </div>  
-                </div>
-                <blog-mansory></blog-mansory>
-              </div>
-        </div> -->
         <div class="content-wrapper dashboard-wrapper">
             <section class="content">
             <div class="box" style="padding:30px;">
             <div class="row">
 						<div class="col-md-12">
-							<!-- TABS -->
 							<ul class="nav nav-tabs nav-tabs-shop-devin" role="tablist">
 								<li class="active" data-wow-delay="0.6s"><a href="#project" data-toggle="tab" aria-expanded="false"><h2><strong>Project</strong></h2></a></li>
 								<li class="" data-wow-delay="0.4s"><a href="#post" data-toggle="tab" aria-expanded="true"><h2><strong>Post</strong></h2></a></li>
 								<li class="" data-wow-delay="0.2s"><a href="#portfolio" data-toggle="tab" aria-expanded="false"><h2><strong>Portfolio</strong></h2></a></li>
 							</ul>
-
 							<div class="tab-content">
-								<!-- TAB PANEL ITEM-->
 								<div role="tabpanel" class="tab-pane fade" id="project">
 									<project-page></project-page>
 								</div>
-								<!--/ TAB PANEL ITEM -->
-
-								<!-- TAB PANEL ITEM-->
 								<div role="tabpanel" class="tab-pane fade active in" id="post">
 									<post-page></post-page>
 								</div>
-								<!--/ TAB PANEL ITEM -->
-
-								<!-- TAB PANEL ITEM-->
 								<div role="tabpanel" class="tab-pane fade" id="portfolio">
 									<portfolio-page></portfolio-page>
 								</div>
-								<!--/ TAB PANEL ITEM -->
 							</div>
-							<!-- / TABS -->
 						</div>
 					  </div>
             </div>
             </section>
-            <!-- /.content -->
         </div>
     </transition>
 </template>
