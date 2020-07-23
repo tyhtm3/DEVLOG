5c772c3909f7a88fed73238dab9311a4572d2e79
c9boom7 <c9boom7@naver.com>
2020-07-23 13:57:27 +0900

    Fix typo
    user_seq -> seq

--- backend/src/main/java/com/ssafy/devlog/dto/Blog.java
+++ backend/src/main/java/com/ssafy/devlog/dto/Blog.java
@@ -3,7 +3,6 @@ package com.ssafy.devlog.dto;
 public class Blog {
 	private int seq;
 	private String blog_name;
-	private int seq_user;
 	private String blog_detail;
 	public int getSeq() {
 		return seq;
@@ -17,12 +16,6 @@ public class Blog {
 	public void setBlog_name(String blog_name) {
 		this.blog_name = blog_name;
 	}
-	public int getSeq_user() {
-		return seq_user;
-	}
-	public void setSeq_user(int seq_user) {
-		this.seq_user = seq_user;
-	}
 	public String getBlog_detail() {
 		return blog_detail;
 	}
@@ -31,15 +24,14 @@ public class Blog {
 	}
 	public Blog() {
 	}
-	public Blog(int seq, String blog_name, int seq_user, String blog_detail) {
+	public Blog(int seq, String blog_name, String blog_detail) {
 		this.seq = seq;
 		this.blog_name = blog_name;
-		this.seq_user = seq_user;
 		this.blog_detail = blog_detail;
 	}
 	@Override
 	public String toString() {
-		return "Blog [seq=" + seq + ", blog_name=" + blog_name + ", seq_user=" + seq_user + ", blog_detail="
+		return "Blog [seq=" + seq + ", blog_name=" + blog_name + ", blog_detail="
 				+ blog_detail + "]";
 	}
 	
