9c3fed95e5c109f3f91744a740b3292ef60ad3da
juhyeon <tab1200@naver.com>
2020-07-24 13:05:54 +0900

    S03P12A402-44 Fix PostController

--- backend/src/main/java/com/ssafy/devlog/controller/PostController.java
+++ backend/src/main/java/com/ssafy/devlog/controller/PostController.java
@@ -36,29 +36,29 @@ public class PostController {
 	private PostService postService;
 	
 	
-	/* 	비로그인 	: params.get("seq_user")==null 로 판별하여 공개범위가 (전체)인 포스트만 반환
-	 * 	로그인 	: 						!=null 로 판별하여 공개범위가 (전체) 포스트 or 공개범위가 (이웃공개/비공개)인 포스트의 접근여부 판별하여 반환  */
+	/* 	비로그인 (seq_user==0) : 공개범위가 '전체'인 포스트 반환
+	 * 	   로그인 (seq_user!=0) : 공개범위가 '전체'인 포스트와 '이웃공개/비공개'인 포스트의 접근여부 판별하여 반환  */
+	
 	
 	
 	/* show feed post */
-	@ApiOperation(value = "피드에서 모든 포스트 반환", response = List.class)
-	@GetMapping(value = "/feed/post")
-	public ResponseEntity<List<Post>> selectAllPost(@RequestBody Map<String, Integer> params) throws Exception {
+	@ApiOperation(value = "피드에서 모든 포스트 반환.", response = List.class)
+	@GetMapping(value = "/feed/post/{seq_user}")
+	public ResponseEntity<List<Post>> selectAllPost(@PathVariable int seq_user) throws Exception {
 		logger.debug("selectAllPost - 호출");
-		int seq_user = (params.get("seq_user")==null?0:params.get("seq_user"));
 		return new ResponseEntity<List<Post>>(postService.selectAllPost(seq_user), HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "피드에서 이웃 블로그의 모든 포스트를 반환", response = List.class)
+	@ApiOperation(value = "피드에서 이웃 블로그의 모든 포스트를 반환.", response = List.class)
 	@GetMapping(value = "/feed/post/neighbor/{seq_user}")
 	public ResponseEntity<List<Post>> selectAllPostByNeighbor(@PathVariable int seq_user) throws Exception {
 		logger.debug("selectAllPostByNeighbor - 호출");
 		return new ResponseEntity<List<Post>>(postService.selectAllPostByNeighbor(seq_user), HttpStatus.OK);
 	}
 
-	
-	@ApiOperation(value = "피드에서 선택된 태그를 가진 모든 포스트를 반환", response = List.class)    
-	@GetMapping(value = "/feed/post/tag")
+	// Map을 받아오기 때문에 Get 대신 Post를 사용함.
+	@ApiOperation(value = "피드에서 선택된 태그를 가진 모든 포스트를 반환. (ex. 'seq_user':1 , 'tag': {'java','mysql'} )", response = List.class)    
+	@PostMapping(value = "/feed/post/tag")
 	public ResponseEntity<List<Post>> selectAllPostByTag(@RequestBody Map<String, Object> params) {
 		logger.debug("selectAllPostByTag - 호출");
 		int seq_user = (params.get("seq_user")==null?0:(int)params.get("seq_user"));
@@ -69,27 +69,28 @@ public class PostController {
 
 	
 	
+	
+	
 	/* show blog post */
-	@ApiOperation(value = "블로그 메인에서 모든 포스트를 반환", response = List.class)    
-	@GetMapping(value = "/blog/post")
-	public ResponseEntity<List<Post>> selectAllPostByBlog(@RequestBody Map<String, Integer> params) {
+	@ApiOperation(value = "블로그 메인에서 모든 포스트를 반환.", response = List.class)    
+	@GetMapping(value = "/blog/post/{seq_blog}/{seq_user}")
+	public ResponseEntity<List<Post>> selectAllPostByBlog(@PathVariable int seq_blog, @PathVariable int seq_user) {
 		logger.debug("selectAllPostByBlog - 호출");
-		int seq_user = (params.get("seq_user")==null?0:params.get("seq_user"));
-		return new ResponseEntity<List<Post>>(postService.selectAllPostByBlog(seq_user, params.get("seq_blog")), HttpStatus.OK);
+		return new ResponseEntity<List<Post>>(postService.selectAllPostByBlog(seq_user, seq_blog), HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "블로그 메인에서 선택한 태그에 해당하는 모든 포스트를 반환", response = List.class)    
-	@GetMapping(value = "/blog/post/tag")
+	// Map을 받아오기 때문에 Get 대신 Post를 사용함.
+	@ApiOperation(value = "블로그 메인에서 선택한 태그에 해당하는 모든 포스트를 반환 (ex. 'seq_user':0 , 'tag': {'java','mysql'} )", response = List.class)    
+	@PostMapping(value = "/blog/post/tag")
 	public ResponseEntity<List<Post>> selectAllPostByBlogByTag(@RequestBody Map<String, Object> params) {
 		logger.debug("selectAllPostByBlogByTag - 호출");
 		int seq_user = (params.get("seq_user")==null?0:(int)params.get("seq_user"));
 		return new ResponseEntity<List<Post>>(postService.selectAllPostByBlogByTag(seq_user,(int)params.get("seq_blog"),(List<String>)params.get("tag")), HttpStatus.OK);
 	}
 	
-	
-	
+
 	/* basic post crud */
-	@ApiOperation(value = "글번호에 해당하는 게시글을 반환한다", response = String.class)
+	@ApiOperation(value = "글번호에 해당하는 게시글을 반환", response = String.class)
 	@GetMapping(value = "/post/{seq}")
 	public ResponseEntity<Post> selectPost(@PathVariable int seq) {
 		logger.debug("selectPost - 호출");
@@ -97,33 +98,37 @@ public class PostController {
 		
 	}
 	
-	@ApiOperation(value = "새로운 게시글을 입력한다.", response = String.class)
+	@ApiOperation(value = "새로운 게시글 입력 ( seq_blog, title, disclosure, content ) 만 사용 ", response = String.class)
 	@PostMapping(value = "/post")
 	public ResponseEntity<String> insertPost(@RequestBody Post post) {
+		
 		logger.debug("insertPost - 호출");
+		
+		// insertPost 이후 postBasic 객체에 seq 받아오기 위한 작업
 		Post postBasic = new Post();
-		postBasic.setSeq(postService.insertPost(post));
+		postBasic.setSeq_blog(post.getSeq_blog());
+		postBasic.setTitle(post.getTitle());
+		postBasic.setDisclosure(post.getDisclosure());
 		postBasic.setContent(post.getContent());
+		postService.insertPost(postBasic);
+		
 		if (postService.insertPostBasic(postBasic)==1) {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 
-	@ApiOperation(value = "게시글을 수정한다.", response = String.class)
+	@ApiOperation(value = "게시글 수정 ( seq, title, disclosure, content ) 만 사용", response = String.class)
 	@PutMapping(value = "/post")
 	public ResponseEntity<String> updatePost(@RequestBody Post post) {
 		logger.debug("updatePost - 호출");
-		Post postBasic = new Post();
-		postBasic.setSeq(postService.updatePost(post));
-		postBasic.setContent(post.getContent());
-		if (postService.updatePostBasic(postBasic)==1) {
+		if (postService.updatePost(post)==1&&postService.updatePostBasic(post)==1) {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
 	}
 
-	@ApiOperation(value = "글번호에 해당하는 게시글을 삭제한다.", response = String.class)
+	@ApiOperation(value = "글번호에 해당하는 게시글 삭제", response = String.class)
 	@DeleteMapping(value = "/post/{seq}")
 	public ResponseEntity<String> deletePost(@PathVariable int seq) {
 		logger.debug("deletePost - 호출");
