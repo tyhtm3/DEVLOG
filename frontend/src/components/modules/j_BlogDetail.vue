5ed6209c2f8d909e1187719d689fc00d635a525a
myeongji <ji_exitosa@naver.com>
2020-07-27 01:34:03 +0900

    S03P12A402-63 Fix detail page layout

--- frontend/src/components/modules/j_BlogDetail.vue
+++ frontend/src/components/modules/j_BlogDetail.vue
@@ -30,12 +30,57 @@
                 </div>
               </div>
               <!-- end profile -->
-              <!-- start body -->
-                <div class="box" style="padding:30px;">
-                {{content}}
-                </div>
-                <!-- /.content -->
-              <!-- end body -->
+              <!-- start content -->
+              <div class="box" style="padding:30px;">
+              {{content}}
+              </div>
+              <!-- end content -->
+              <!-- start tag -->
+              <div class="box" style="padding:30px;">
+                <!-- tags에 태그 리스트 받아온뒤 포문으로 출력-->
+                <!-- 사이즈 정돈 필요 -->
+                <span v-for="(tag, index) in tags" v-bind:key="index">
+                <span class="tag" >#{{tag}} </span>
+                </span>
+              </div>
+              <!-- end tag -->
+              <!-- start comment -->
+              <div class="comment-nest">
+                  <el-input type="textarea" :rows="2" placeholder="바른말 고운말~ 기분 좋아지는 댓글을 작성해주세요 *^^*" v-model="textarea"> </el-input>
+                  <br><br>
+                  <el-button style="float:right">댓글 등록</el-button><br><br>
+                  <hr>
+                  <ul class="media-list">
+                    <li class="media">
+                        <a class="pull-left" href="#"> <img class="media-object img-circle" data-src="holder.js/64x64" alt="64x64" src="http://api.randomuser.me/portraits/thumb/women/21.jpg" style="width: 40px; height: 40px;"> </a>
+                        <div class="media-body">
+                          <div class="social-profile">
+                              <h3> 
+                                <a class="link-comment" href="#">손명묭1호팬</a>
+                                <span><i class="entypo-globe"></i>&nbsp;1 hours ago</span>
+                              </h3> 
+                          </div>
+                          <p>잘 보고갑니다~</p>
+                        </div>
+                    </li>
+                  </ul>
+                  <hr>
+                  <ul class="media-list">
+                      <li class="media">
+                          <a class="pull-left" href="#"> <img class="media-object img-circle" data-src="holder.js/64x64" alt="64x64" src="http://api.randomuser.me/portraits/thumb/women/9.jpg" style="width: 40px; height: 40px;"> </a>
+                          <div class="media-body">
+                            <div class="social-profile">
+                                <h3> 
+                                  <a class="link-comment" href="#">손명묭2호팬</a>
+                                  <span><i class="entypo-globe"></i>&nbsp;22 hours ago</span>
+                                </h3> 
+                            </div>
+                            <p>명묭언니 팬이에요~~~~</p>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+              <!-- end comment -->
             </div>
             <!-- end movie card -->
           </div>
@@ -64,6 +109,8 @@
           comments: '17',
           like_count: '523',
           content:'내용없다',
+          // tags : 태그 리스트 불러오는거 없나!?
+          tags: ['java', 'spring', 'python', 'aws', 'ml', 'database', 'blockchain', 'javascript', 'tensorflow'],
         }
     },
     methods: {
