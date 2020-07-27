8c8b7b57f362f7a23af71a568c08ecfc927a36dc
c9boom7 <c9boom7@naver.com>
2020-07-27 14:13:02 +0900

    S03P12A402-78 Add CRUD,ProfileImg Upload to User
    Need to file upload test

---
+++ backend/src/main/java/com/ssafy/devlog/service/UserService.java
@@ -0,0 +1,13 @@
+package com.ssafy.devlog.service;
+
+import java.util.List;
+
+import com.ssafy.devlog.dto.User;
+
+public interface UserService {
+	public List<User> selectAllUser();
+	public User selectUserBySeq(int seq);
+	public int insertUser(User user);
+	public int updateUser(User user);
+	public int deleteUser(int seq);
+}
