d89d38e3634d6ee2e2ef3f894eff63483b0c6587
juhyeon <tab1200@naver.com>
2020-07-27 10:15:02 +0900

    S03P12A402-74 Add crud to projectTechStack

---
+++ backend/src/main/java/com/ssafy/devlog/service/ProjectStackService.java
@@ -0,0 +1,13 @@
+package com.ssafy.devlog.service;
+
+import java.util.List;
+
+import com.ssafy.devlog.dto.PostTag;
+import com.ssafy.devlog.dto.ProjectStack;
+
+public interface ProjectStackService {
+	
+	public List<ProjectStack> selectAllProjectStack(int seq_post_project);
+	public int insertProjectStack(ProjectStack projectStack);
+	public int deleteProjectStack(int seq);
+}
