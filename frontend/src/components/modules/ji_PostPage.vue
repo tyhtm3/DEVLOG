7dd7650559f5a320ab6d50974cddb68f88af3c31
myeongji <ji_exitosa@naver.com>
2020-07-27 23:47:50 +0900

    S03P12A402-83 S03P12A402-84 Fix bottom part of the blog main page to show real data

--- frontend/src/components/modules/ji_PostPage.vue
+++ frontend/src/components/modules/ji_PostPage.vue
@@ -1,54 +1,58 @@
 <template>
     <transition name="el-zoom-in-top">
         <section class="content">
-            포스트출력
-                <!-- <div class="row-mansory">
-                    <div class="col-md-4">
+            <!-- 포스트출력 -->
+                <div class="row">
+                     <div class="col-md-4" v-for="d in datas" :key="d">
                         <div class="well-media">
+                        <a href="#">
                             <div class="vendor">
-                                <div class="fluid-width-video-wrapper embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="#" frameborder="0" allowfullscreen="" id="fitvid892886"></iframe>
-                                </div>
+                                <img class="img-responsive-media" src="https://www.bloter.net/wp-content/uploads/2014/05/unreal_1_600.jpg" alt="">
+                                <!-- <a class="fancybox" rel="group" href="#"> <img class="img-responsive-media" src="https://www.bloter.net/wp-content/uploads/2014/05/unreal_1_600.jpg" alt=""> </a> -->
                             </div>
                             <div class="video-text">
-                                <h2>Black Angel 1</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dui id justo rhoncus consequat. Aliquam porttitor, arcu quis ornare ultricies, augue neque fringilla turpis.</p>
+                                <h2>{{d}}</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </div>
                             <div class="tag-nest"> <i>angel</i><i>dark</i><i>mistic</i> </div>
                             <div class="video-category-bg">
-                                <h3>Video</h3>
-                                <a class="link-media pull-right" href="#"> <span class="fontawesome-film"></span> </a>
+                                <h3>FRONT-END</h3>
+                                <a class="link-media pull-right" href="#"> <span class="fontawesome-picture"></span> </a>
                                 <div class="triangle-white"></div>
-                                <div class="triangle-video-right"></div>
+                                <div class="triangle-photo-right"></div>
                             </div>
+                        </a>
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <div class="well-media">
-                            <div class="vendor">
-                                <div class="fluid-width-video-wrapper embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="" frameborder="0" allowfullscreen="" id="fitvid892886"></iframe>
-                                </div>
-                            </div>
-                            <div class="video-text">
-                                <h2>Black Angel 8</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dui id justo rhoncus consequat. Aliquam porttitor, arcu quis ornare ultricies, augue neque fringilla turpis.</p>
-                            </div>
-                            <div class="tag-nest"> <i>angel</i><i>dark</i><i>mistic</i> </div>
-                            <div class="video-category-bg">
-                                <h3>Video</h3>
-                                <a class="link-media pull-right" href="#"> <span class="fontawesome-film"></span> </a>
-                                <div class="triangle-white"></div>
-                                <div class="triangle-video-right"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
+                </div>
             </section>
     </transition>
 </template>
 <script>
   export default {
-    name: 'ji_ProjectPage'
+    name: 'ji_ProjectPage',
+    data(){
+        return{
+            seq_blog: '2',
+            seq_user: '0',
+            postList: [],
+            datas: ['java', 'spring', 'python', 'aws', 'ml', 'database', 'blockchain', 'javascript', 'tensorflow']
+        }
+    },
+    created(){	 
+	  this.getpostList();
+    },
+    methods:{
+        getpostList(){
+            http.get('devlog/api/blog/post/'+this.seq_blog+'/' + this.seq_user)
+            .then(({data}) => {
+				if(data.length != 0) {
+				    this.postList = data;
+				}else{
+				    this.postList = null;
+				}
+			})
+		},
+    }
   }
 </script>
