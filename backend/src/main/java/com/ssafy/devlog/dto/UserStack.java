143a0d0a63748f1c320b04400be33750587af750
c9boom7 <c9boom7@naver.com>
2020-07-28 13:31:22 +0900

    S03P12A402-79 Add CRUD to UserStack

---
+++ backend/src/main/java/com/ssafy/devlog/dto/UserStack.java
@@ -0,0 +1,28 @@
+package com.ssafy.devlog.dto;
+
+public class UserStack {
+	private int seq;
+	private int seq_user;
+	private String stack;
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public int getSeq_user() {
+		return seq_user;
+	}
+	public void setSeq_user(int seq_user) {
+		this.seq_user = seq_user;
+	}
+	public String getStack() {
+		return stack;
+	}
+	public void setStack(String stack) {
+		this.stack = stack;
+	}
+	
+	
+}
