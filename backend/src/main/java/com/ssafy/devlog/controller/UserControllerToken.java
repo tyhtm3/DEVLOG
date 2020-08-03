69285bf5df06de71da9b68231e740b951d26394f
c9boom7 <c9boom7@naver.com>
2020-08-03 17:39:28 +0900

    S03P13A402-26 Add JWTAuth Controller, Application, Filter

---
+++ backend/src/main/java/com/ssafy/devlog/controller/UserControllerToken.java
@@ -0,0 +1,55 @@
+package com.ssafy.devlog.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.devlog.dto.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+public class UserControllerToken {
+
+	@PostMapping("user")
+	public User login(@RequestBody User user) {
+		
+		String id = user.getId();
+		
+		System.out.println(id);
+		String token = getJWTToken(id);
+		User nUser = new User();
+		nUser.setName(id);
+		nUser.setToken(token);		
+		return nUser;
+		
+	}
+
+	private String getJWTToken(String username) {
+		String secretKey = "mySecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+		
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes()).compact();
+
+		return "Bearer " + token;
+	}
+}
