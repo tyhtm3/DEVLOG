0f872d66ddf57646da0aa57da26d3b7b1ac9d733
unknown <oyes9316@naver.com>
2020-08-07 04:33:26 +0900

    Fix error

--- frontend/src/components/writePortfolio.vue
+++ frontend/src/components/writePortfolio.vue
@@ -365,7 +365,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .tocenter{
     vertical-align: middle;    
   }
--- frontend/src/pages/SignUp.vue
+++ frontend/src/pages/SignUp.vue
@@ -139,13 +139,7 @@ import http from '../util/http-common'
         if(month<10) month = '0' + month;
         var date = format.getDate();
         if(date<10) date = '0' + date;
-        var hour = format.getHours();
-        if(hour<10) hour = '0' + hour;
-        var min = format.getMinutes();
-        if(min<10) min = '0' + min;
-        var sec = format.getSeconds();
-        if(sec<10) sec = '0' + sec;
-        return year + "-" + month + "-" + date + " " + hour + ":" + min + ":" + sec;}
+        return year + "-" + month + "-" + date}
       },
      handleAvatarSuccess(res, file) {
       var frm = new FormData();
