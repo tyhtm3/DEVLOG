678ed552f69f9f12b2b6c8e9f375551fc2d07498
juhyeon <tab1200@naver.com>
2020-08-13 22:22:51 +0900

    Fix portfolio insert and update sql

--- backend/src/main/java/com/ssafy/devlog/dto/Portfolio.java
+++ backend/src/main/java/com/ssafy/devlog/dto/Portfolio.java
@@ -23,6 +23,21 @@ public class Portfolio {
 	private String github_url;
 	private String birthday;
 	private String content;
+	private String tel;
+	private String email;
+	
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	public int getSeq() {
 		return seq;
 	}
@@ -94,7 +109,7 @@ public class Portfolio {
 		return "Portfolio [seq=" + seq + ", seq_blog=" + seq_blog + ", title=" + title + ", regtime=" + regtime
 				+ ", disclosure=" + disclosure + ", like_count=" + like_count + ", img_url=" + img_url + ", name="
 				+ name + ", profile_img_url=" + profile_img_url + ", github_url=" + github_url + ", birthday="
-				+ birthday + ", content=" + content + "]";
+				+ birthday + ", content=" + content + ", tel=" + tel + ", email=" + email + "]";
 	}
 
 	
