ccc7a0b44016b7695f6142db6dc3040a1ad6d325
c9boom7 <c9boom7@naver.com>
2020-08-13 13:14:30 +0900

    S03P13A402-106 Fix design to pie-chart

--- frontend/src/pages/TemplateYS.vue
+++ frontend/src/pages/TemplateYS.vue
@@ -4,169 +4,188 @@
 
 <div id="container">
   <div id ="main">
-    <span id="profile_img">
-    <img src='http://i3a402.p.ssafy.io/images/20200803062316_profile.jpg'>
-    </span>
-    <span>
-    이름       : 차윤석( 24세 )
-    </span>
-    <span>
-    관심분야   : Backend, Server
-    </span>
-    <span>
-    Github    : https://github.com/C9Boom7
-    </span>
-    <br>
-        <!-- Main content -->
-        <section class="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <!-- Default box -->
-                    <div class="box">
-                        <div class="box-body">
-                            <ve-pie :data="chartData" :settings="chartSettings"></ve-pie>
-                        </div>
-                        <!-- /.box-body -->
-                    </div>
-                </div>
-            </div>
-             <div class="box-body no-pad padding-fifthin" style="display: block;">
-                <div class="events-nest ">
-                    <div class="wrap-no-pad padding-fifthin">
-                        <div class="row">
-                            <div class="col-sm-6 bor-right bor-bottom">
-                                <div class="devin-table-dash centered pad-box"> <i class="palette-Pink-500 bg ti-shopping-cart-full"></i>
-                                    <div class="palette-Pink-500 text value">648</div>
-                                    <div class="label">Items Sold</div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6 bor-bottom">
-                                <div class="devin-table-dash centered pad-box"> <i class="palette-Purple-500 bg ti-link"></i>
-                                    <div class="palette-Purple-500 text value">47.5<strong>K</strong></div>
-                                    <div class="label">Followers</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6 bor-right">
-                                <div class="devin-table-dash centered pad-box"> <i class="palette-Deep-Purple-500 bg ti-wallet"></i>
-                                    <div class="value palette-Deep-Purple-500 text"><strong>$</strong>764</div>
-                                    <div class="label">Daily Earnings</div>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="devin-table-dash centered pad-box"> <i class="ti-bag palette-Light-Blue-500 bg"></i>
-                                    <div class="value palette-Light-Blue-500 text">256</div>
-                                    <div class="label">Products</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-  </div>
-  <div class="box-body" style="min-height:400px;">
-        <div class="container-timeline">
-            <div class="header-timeline">
-                <svg xmlns="http://www.w3.org/2000/svg" height="64" width="64">
-                    <defs>
-                        <linearGradient id="verlauf" x1="0%" y1="0%" x2="100%" y2="100%">
-                            <stop offset="0%" stop-color="orange" />
-                            <stop offset="100%" stop-color="chocolate" /> </linearGradient>
-                    </defs>
-                    <path fill="url(#verlauf)" d="M63.04 39.74C58.764 56.885 41.4 67.318 24.256 63.043 7.12 58.768-3.313 41.404.962 24.262 5.234 7.117 22.597-3.318 39.737.957 56.88 5.23 67.313 22.597 63.04 39.74z" />
-                    <path fill="#FFF" d="M46.11 27.44c.636-4.257-2.606-6.546-7.04-8.073l1.44-5.768-3.512-.876-1.4 5.616c-.923-.23-1.87-.447-2.813-.662l1.41-5.653-3.51-.875-1.438 5.766c-.764-.174-1.514-.346-2.242-.527l.004-.02-4.843-1.208-.934 3.75s2.605.597 2.55.634c1.422.355 1.68 1.296 1.636 2.042l-1.64 6.57c.1.026.226.062.366.118l-.37-.092-2.297 9.205c-.175.43-.616 1.08-1.61.833.035.05-2.552-.637-2.552-.637l-1.743 4.02 4.57 1.138c.85.213 1.682.436 2.502.646l-1.453 5.834 3.507.875 1.44-5.773c.958.26 1.888.5 2.798.726l-1.434 5.745 3.51.875 1.454-5.823c5.987 1.133 10.49.676 12.384-4.74 1.527-4.36-.076-6.874-3.226-8.514 2.294-.53 4.022-2.038 4.483-5.155zm-8.023 11.25c-1.085 4.36-8.426 2.003-10.806 1.412l1.93-7.73c2.38.595 10.01 1.77 8.877 6.318zm1.086-11.312c-.99 3.966-7.1 1.95-9.082 1.457l1.75-7.01c1.98.494 8.364 1.416 7.333 5.553z" /> </svg>
-                <h1>History of bitcoin</h1>
-                <h2>A timeline that shows bitcoin History from the idea all the way to present day.</h2> </div>
-            <div class="item-timeline">
-                <div id="timeline">
-                    <div>
-                        <section class="year">
-                            <h3>2007</h3>
-                            <section>
-                                <ul>
-                                    <li>Satoshi Nakamoto began working on the Bitcoin concept.</li>
-                                </ul>
-                            </section>
-                        </section>
-                        <section class="year">
-                            <h3>2016</h3>
-                            <section>
-                                <h4>January</h4>
-                                <ul>
-                                    <li>Price at <span class="price">$1150</span> per bitcoin.</li>
-                                    <li>Price fell 30% in a week, reaching a multi-month low of <span class="price">$750.</span></li>
-                                    <li>The network exceeded 1 exahash/sec.</li>
-                                </ul>
-                            </section>
-                            <section>
-                                <h4>February</h4>
-                                <ul>
-                                    <li>Version 0.12 released.</li>
-                                </ul>
-                            </section>
-                            <section>
-                                <h4>April</h4>
-                                <ul>
-                                    <li>Steam started accepting bitcoin as payment.</li>
-                                </ul>
-                            </section>
-                            <section>
-                                <h4>August</h4>
-                                <ul>
-                                    <li>Version 0.13 released.</li>
-                                </ul>
-                            </section>
-                            <section>
-                                <h4>September</h4>
-                                <ul>
-                                    <li>There are 770 bitcoin ATMs worldwide.</li>
-                                </ul>
-                            </section>
-                        </section>
-                        <section class="year">
-                            <h3>2017</h3>
-                            <section>
-                                <h4>March</h4>
-                                <ul>
-                                    <li>Version 0.14 released.</li>
-                                    <li>The price of 1 bitcoin surpassed the spot price of an ounce of gold for the first time.</li>
-                                    <li>The number of GitHub projects related to bitcoin passed 10,000.</li>
-                                    <li>Price traded above <span class="price">$1290.</span></li>
-                                </ul>
-                            </section>
-                            <section>
-                                <h4>Today</h4>
-                                <ul>
-                                    <li>At the moment, one bitcoin cost: <span class="price"><div id="btcwidget"></div>.</span></li>
-                                </ul>
-                            </section>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </div>  
+    <br><br><br><br>
+    <ul>
+      <li>
+    <div id="profile_img">
+    <img class="pull-left" alt="profile_img" src='http://i3a402.p.ssafy.io/images/20200803062316_profile.jpg'>
     </div>
-
-
-</div>
+      <li>
+    <li>&emsp;이름       : 차윤석( 24세 )</li>
+    <li>&emsp;관심분야   : Backend, Server</li>
+    <li>&emsp;Github    : https://github.com/C9Boom7</li>
+    </ul>
+    <br>
+      <!-- Main content -->
+    <section class="content">
+      <div class="row">
+          <div class="col-md-12">
+              <!-- Default box -->
+              <div class="box">
+                  <div class="box-body" id="box-pie">
+                      <ve-pie :data="chartData" :settings="chartSettings"></ve-pie>
+                  </div>
+                  <!-- /.box-body -->
+              </div>
+          </div>
+      </div>
+      <div class="box-body no-pad padding-fifthin" style="display: block;">
+        <div class="events-nest ">
+          <div class="wrap-no-pad padding-fifthin">
+            <div class="row">
+              <div class="col-sm-6 bor-right bor-bottom">
+                <div class="devin-table-dash centered pad-box"> <i class="palette-Pink-500 bg ti-shopping-cart-full"></i>
+                  <div class="palette-Pink-500 text value">648</div>
+                  <div class="label">Items Sold</div>
+                </div>
+              </div>
+              <div class="col-sm-6 bor-bottom">
+                <div class="devin-table-dash centered pad-box"> <i class="palette-Purple-500 bg ti-link"></i>
+                  <div class="palette-Purple-500 text value">47.5<strong>K</strong></div>
+                  <div class="label">Followers</div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 bor-right">
+                <div class="devin-table-dash centered pad-box"> <i class="palette-Deep-Purple-500 bg ti-wallet"></i>
+                  <div class="value palette-Deep-Purple-500 text"><strong>$</strong>764</div>
+                  <div class="label">Daily Earnings</div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="devin-table-dash centered pad-box"> <i class="ti-bag palette-Light-Blue-500 bg"></i>
+                  <div class="value palette-Light-Blue-500 text">256</div>
+                  <div class="label">Products</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   
+  <div class="box-body" style="min-height:400px;">
+    <div class="container-timeline">
+      <div class="header-timeline">
+        <img src="http://i3a402.p.ssafy.io/images/project.png" height="64" width="64">
+        <h1>프로젝트</h1>
+        <h2>A timeline that shows my project history</h2> </div>
+        <div class="item-timeline">
+          <div id="timeline">
+            <div>
+              <section class="year" v-for="(project,index) in projects" :key="index">
+                <h3>{{project.start_date.substr(0,7)}}</h3>
+                <section>
+                  <ul>
+                    <li><img :src="project.img_url" height="250" width="400"></li>
+                    
+                    <li>프로젝트명 : {{project.title}}</li>
+                    <li>img url : {{project.img_url}}</li>
+                    <li>개발기간 : {{project.start_date}}~{{project.finish_date}}</li>
+                    <li>프로젝트요약 : {{project.summary}}</li>
+                    <li>역할 : 
+                      <ul>
+                        <li>to-do 페이지 제작</li>
+                        <li>데이터베이스 설계</li>
+                        <li>FireBase 연동</li>
+                      </ul>
+                    </li>
+                    <li>기술스택 : 
+                      <div v-for="(stack,index) in stacks" :key="index">
+                        <img class="media-object img-circle pull-left" :alt="stack.stack" :src="stack.stack_img_url" style="width: 64px; height: 64px;margin-right:20px;">
+                      </div>
+                    </li>
+                    <li>링크 : <a href="http://i3a402.p.ssafy.io">http://i3a402.p.ssafy.io</a> </li>
+                    <li>자세히보기 : <a href="http://localhost:8080/blog/project/394">http://localhost:8080/blog/project/394</a> </li>  
+                  </ul>
+                </section>
+              </section>
+              <section class="year">
+                <h3>2016</h3>
+                <section>
+                  <h4>January</h4>
+                  <ul>
+                    <li>Price at <span class="price">$1150</span> per bitcoin.</li>
+                    <li>Price fell 30% in a week, reaching a multi-month low of <span class="price">$750.</span></li>
+                    <li>The network exceeded 1 exahash/sec.</li>
+                  </ul>
+                </section>
+                <section>
+                  <h4>February</h4>
+                  <ul>
+                    <li>Version 0.12 released.</li>
+                  </ul>
+                </section>
+                <section>
+                  <h4>April</h4>
+                  <ul>
+                    <li>Steam started accepting bitcoin as payment.</li>
+                  </ul>
+                </section>
+                <section>
+                  <h4>August</h4>
+                  <ul>
+                    <li>Version 0.13 released.</li>
+                  </ul>
+                </section>
+                <section>
+                  <h4>September</h4>
+                  <ul>
+                    <li>There are 770 bitcoin ATMs worldwide.</li>
+                  </ul>
+                </section>
+              </section>
+              <section class="year">
+                <h3>2017</h3>
+                <section>
+                  <h4>March</h4>
+                  <ul>
+                    <li>Version 0.14 released.</li>
+                    <li>The price of 1 bitcoin surpassed the spot price of an ounce of gold for the first time.</li>
+                    <li>The number of GitHub projects related to bitcoin passed 10,000.</li>
+                    <li>Price traded above <span class="price">$1290.</span></li>
+                  </ul>
+                  </section>
+                  <section>
+                    <h4>Today</h4>
+                    <ul>
+                      <li>At the moment, one bitcoin cost: <span class="price"><div id="btcwidget"></div>.</span></li>
+                    </ul>
+                  </section>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+  </div>
+  </div>
 </section>
-
+ 
 </template>
 
 <script>
-export default{
-  date(){
+
+import http from '../util/http-common'
+export default {
+  data: function () {
+    return {
+      seq_portfolio: 395,
+      chartData : {},
+      chartSettings : {},
+      projects : [],
+      stacks : [],
+    }
   },
    mounted(){
     $(".main-header").css('display','none')
     $(".main-footer"). css('display','none')
   },
-  created: function () {
+  created() {
       this.chartData = {
         rows: [
+          { 'stack': 'Spring', 'rate': 5 },
           { 'stack': 'Spring', 'rate': 5 },
           { 'stack': 'Java', 'rate': 4 },
           { 'stack': 'MariaDB', 'rate': 3 },
@@ -178,17 +197,39 @@ export default{
         dimension: 'stack',
         metrics: 'rate'
       }
+      this.stacks = [
+      ]
+      this.getProjectInfo()
+    },
+    methods: {
+      getProjectInfo(){
+        http.get('portfoliopjt/'+this.seq_portfolio)
+        .then(({data}) =>{
+          this.projects = data
+          for(var i=0;i<data.length;i++){
+            http.get('projectstack/'+this.projects[i].seq)
+            .then(({data})=>{
+              for(var j=0;j<data.length;j++){
+                this.stacks.push(data[j])
+              }
+            })
+          }
+        })
+      }
     }
 }
 </script>
 
 <style lang="scss" scoped>
 
+#container{
+  background-color: #E6E6E6;
+}
 #main{
   position: relative;
   margin-left : 20%;
   margin-right : 20%;
-  background-color : #E6E6E6;
+  background-color :white;
 }
 .tl-item {
   transform: translate3d(0, 0, 0);
@@ -294,5 +335,12 @@ export default{
 }
 .padding-fifthin{
   padding: 15px!important;
+}
+ul{
+  list-style: none;
+}
+#box-pie{
+  margin-left: 15%;
+  margin-right: 15%;
 }
 </style>
