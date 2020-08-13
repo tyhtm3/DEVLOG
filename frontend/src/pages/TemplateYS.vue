3455a53ce2359d8ccc332bca02594d41f2e3d603
c9boom7 <c9boom7@naver.com>
2020-08-13 22:18:10 +0900

    S03P13A402-106 Add portfoliotemplete - timeline

--- frontend/src/pages/TemplateYS.vue
+++ frontend/src/pages/TemplateYS.vue
@@ -5,64 +5,71 @@
 <div id="container">
   <div id ="main">
     <br><br><br><br>
-    <ul>
+    <hr>
+    <br>
+    <br>
+    <ul style="margin-left : 20%">
       <li>
     <div id="profile_img">
-    <img class="pull-left" alt="profile_img" src='http://i3a402.p.ssafy.io/images/20200803062316_profile.jpg'>
-    </div>
-      <li>
-    <li>&emsp;이름       : 차윤석( 24세 )</li>
-    <li>&emsp;관심분야   : Backend, Server</li>
+    <img class="pull-left" alt="profile_img" :src='blogOwnerInfo.profile_img_url' width="160px" height="160px">
+    </div>  
+      </li>
+    <br><br>
+    <li>&emsp;이름 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {{blogOwnerInfo.name}}( {{blogOwnerInfo.birthday.substr(0,11)}} )</li>
+    <li>&emsp;연락처 &nbsp;&nbsp;: {{blogOwnerInfo.tel}}</li>
     <li>&emsp;Github    : https://github.com/C9Boom7</li>
     </ul>
     <br>
+    
       <!-- Main content -->
     <section class="content">
+      <hr>
+      <div class="box-body no-pad padding-fifthin" style="display: block;">
+        <div class="events-nest ">
+          <div class="wrap-no-pad padding-fifthin">
+            <div class="row">
+              <div class="col-sm-6 bor-right bor-bottom">
+                <div class="devin-table-dash centered pad-box">
+                  <div class="palette-Pink-500 text value">{{projects.length}}<strong>개</strong></div>
+                  <div class="label">프로젝트</div>
+                </div>
+              </div>
+              <div class="col-sm-6 bor-bottom">
+                <div class="devin-table-dash centered pad-box"> 
+                  <div class="palette-Purple-500 text value">{{totaldays}}<strong>일</strong></div>
+                  <div class="label">개발 기간</div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 bor-right">
+                <div class="devin-table-dash centered pad-box"> 
+                  <div class="value palette-Deep-Purple-500 text">{{chartData.rows.length}}<strong>개</strong></div>
+                  <div class="label">기술 스택</div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="devin-table-dash centered pad-box"> 
+                  <div class="value palette-Light-Blue-500 text">{{likeCount}}<strong>개</strong></div>
+                  <div class="label">좋아요</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr>
       <div class="row">
           <div class="col-md-12">
               <!-- Default box -->
               <div class="box">
                   <div class="box-body" id="box-pie">
                       <ve-pie :data="chartData" :settings="chartSettings" :id="box-pie"></ve-pie>
-                      <h4 style="text-align : center">프로젝트 기술스택 목록</h4>
+                      <h4 style="text-align : center">Project TechStack</h4>
                   </div>
                   <!-- /.box-body -->
               </div>
           </div>
-      </div>
-      <div class="box-body no-pad padding-fifthin" style="display: block;">
-        <div class="events-nest ">
-          <div class="wrap-no-pad padding-fifthin">
-            <div class="row">
-              <div class="col-sm-6 bor-right bor-bottom">
-                <div class="devin-table-dash centered pad-box"> <i class="palette-Pink-500 bg ti-shopping-cart-full"></i>
-                  <div class="palette-Pink-500 text value">648</div>
-                  <div class="label">Items Sold</div>
-                </div>
-              </div>
-              <div class="col-sm-6 bor-bottom">
-                <div class="devin-table-dash centered pad-box"> <i class="palette-Purple-500 bg ti-link"></i>
-                  <div class="palette-Purple-500 text value">47.5<strong>K</strong></div>
-                  <div class="label">Followers</div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-6 bor-right">
-                <div class="devin-table-dash centered pad-box"> <i class="palette-Deep-Purple-500 bg ti-wallet"></i>
-                  <div class="value palette-Deep-Purple-500 text"><strong>$</strong>764</div>
-                  <div class="label">Daily Earnings</div>
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="devin-table-dash centered pad-box"> <i class="ti-bag palette-Light-Blue-500 bg"></i>
-                  <div class="value palette-Light-Blue-500 text">256</div>
-                  <div class="label">Products</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   
@@ -71,7 +78,11 @@
       <div class="header-timeline">
         <img src="http://i3a402.p.ssafy.io/images/project.png" height="64" width="64">
         <h1>프로젝트</h1>
-        <h2>A timeline that shows my project history</h2> </div>
+        <h2>A timeline that shows my project history</h2>
+        <br>
+        <br>
+        <hr>
+      </div>
         <div class="item-timeline">
           <div id="timeline">
             <div>
@@ -144,6 +155,7 @@
 <script>
 
 import http from '../util/http-common'
+
 export default {
   data: function () {
     return {
@@ -152,6 +164,8 @@ export default {
       chartSettings : {},
       projects : [],
       blogOwnerInfo : {},
+      likeCount : 0,
+      totaldays : 0,
     }
   },
    mounted(){
@@ -176,15 +190,20 @@ export default {
       ]
       this.getProjectInfo()
     },
+    
     methods: {
       //포폴 정보 가져오기
       getProjectInfo(){
         http.get('portfoliopjt/'+this.seq_portfolio)
         .then(({data}) =>{
           this.projects = data
-          console.log(this.projects.seq_blog)
           // 프로젝트 개수 반복
           for(var i=0;i<data.length;i++){
+            //좋아요 개수 추가
+            this.likeCount += data[i].like_count
+            console.log(this.likeCount)
+            //개발기간 추가
+            this.totaldays += (dateDiff(data[i].finish_date, data[i].start_date))
             //스택 개수 반복
             for(var j=0;j<data[i].stacks.length;j++){
               var rows = new Object()
@@ -204,16 +223,29 @@ export default {
               this.chartData.rows.push(rows)
             } 
           }
-          http.get('/user/'+this.projects.seq_blog)
+          http.get('/user/'+this.projects[0].seq_blog)
           .then(({data})=>{
             this.blogOwnerInfo = data;
-            console.log(data)
           })
+          this.projects.sort(this.customSort)
         })
-      }             
-    }
-    }
-    
+      },
+      customSort(a, b) {
+        if(a.start_date == b.start_date){
+          return 0
+          }
+          return a.start_date > b.start_date ? 1 : -1; }             
+      },
+  }
+  function dateDiff(_date1, _date2) {
+    var diffDate_1 = _date1 instanceof Date ? _date1 :new Date(_date1);
+    var diffDate_2 = _date2 instanceof Date ? _date2 :new Date(_date2);
+    diffDate_1 =new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
+    diffDate_2 =new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+    var diff = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+    diff = Math.ceil(diff / (1000 * 3600 * 24));
+    return diff;
+}   
 
 </script>
 
