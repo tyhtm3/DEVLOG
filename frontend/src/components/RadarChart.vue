35f84d68c24365b0ca7bff739b9c09d47d1aeff8
juhyeon <tab1200@naver.com>
2020-08-14 15:02:56 +0900

    S03P13A402-135 Update Template03.vue to add tech stack

---
+++ frontend/src/components/RadarChart.vue
@@ -0,0 +1,42 @@
+
+<script>
+  import { Radar } from 'vue-chartjs'
+
+  export default {
+    extends: Radar,
+    props: ['label','exp'],
+    data () {
+      return {
+        chartData: {
+          labels: [
+             "Babol",	
+             "Cabanatuan",	
+             "Daegu",	
+             "Jerusalem",	
+             "Fairfield",	
+          ],
+          datasets: [
+            {
+              label: 'Ability',
+              borderWidth: 1,
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+             
+              data: [10,40,100]
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    },
+    created(){
+       this.chartData.labels=this.label
+       
+    },
+    mounted () {
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+</script>
